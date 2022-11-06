59926f3d0cb52b265adb3b197df9c98f3b0c5ab6
JonasHolmen123 <Altforski@gmail.com>
2022-11-06 23:20:35 +0100

    rev2

--- stlGen.py
+++ stlGen.py
@@ -3,8 +3,8 @@ from scipy.ndimage import gaussian_filter
 from pylab import imread
 
 
-A = imread("/home/jonas/MAS417_project/MAS417_project/clearsky_day.png") # read from rendered png
+A = imread("/home/jonas/MAS417_project/MAS417_project/clearsky_day.png",0) # read from rendered png
 A = A.mean(axis=2) #grayscale projection
-A = gaussian_filter(A.max() - A, 1.)
+A = gaussian_filter(A, 1)
 
 numpy2stl(A, "/home/jonas/MAS417_project/MAS417_project/test.stl", scale=0.05, solid=True, ascii=False)
