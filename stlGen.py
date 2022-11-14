82b5e808323d23336a41b899c1e0149104c30d85
JonasHolmen123 <Altforski@gmail.com>
2022-11-14 12:45:27 +0100

    stl fix

--- stlGen.py
+++ stlGen.py
@@ -10,9 +10,9 @@ class stlGen:
     def generate_png(self, text):
         self.text = text
         text = f"{self.text}"
-        text2png(text,"/home/jonas/MAS417_project/MAS417_project/textPNG", fontsize = 50)
+        text2png(text,"/home/jonas/MAS417_project/MAS417_project/textPNG", fontsize = 40)
         A = imread("/home/jonas/MAS417_project/MAS417_project/textPNG.png",2) # read from rendered png
         A = A.mean(axis=2) #grayscale projection
-        A = gaussian_filter(A.max()-A, 2)
-        numpy2stl(A,"/home/jonas/MAS417_project/MAS417_project/weatherData.stl", scale=0.5, solid=True, ascii=False)
+        A = gaussian_filter(A.max()-A, 0.6)
+        numpy2stl(A,"/home/jonas/MAS417_project/MAS417_project/weatherData.stl", scale=0.1, solid=True, ascii=False)
 
